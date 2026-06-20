2185f82473c51d68bcb2b2384620c5f719e7f9af
zenhkm <zenhkm@gmail.com>
2026-06-21 00:19:38 +0700

    test.php v2

--- test.php
+++ test.php
@@ -6,8 +6,10 @@ use App\Config\Database;
 try {
     $pdo = Database::getConnection();
     $stmt = $pdo->query("SHOW COLUMNS FROM books");
-    $columns = $stmt->fetchAll();
-    echo json_encode($columns);
+    $columns = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    foreach ($columns as $c) {
+        echo $c['Field'] . "\n";
+    }
 } catch (Exception $e) {
     echo "Error: " . $e->getMessage();
 }
