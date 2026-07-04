4cd1ac3859bc8a5e3d2cb0f03b47f8d94d5feaab
zenhkm <zenhkm@gmail.com>
2026-07-04 22:50:10 +0700

    fix: Remove UTF-8 BOM from search.php that caused 500 headers already sent error

--- api_android/search.php
+++ api_android/search.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json; charset=utf-8');
 // Sesuaikan path karena file ini berada di dalam folder api_android/
