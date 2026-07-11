1008106c5502429d01a086e5dc7159739734714d
zenhkm <zenhkm@gmail.com>
2026-07-11 09:29:00 +0700

    Remove legacy files from repository

--- test.php
+++
@@ -1,5 +0,0 @@
-<?php
-require 'app/bootstrap.php';
-$pdo = App\Config\Database::getConnection();
-$cols = $pdo->query('DESCRIBE users')->fetchAll(PDO::FETCH_ASSOC);
-foreach($cols as $col) echo $col['Field'] . "\n";
